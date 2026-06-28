f511f814e1638be643cdaf09f83e02ea5bef0097
bthwani2-boop <bthwani2@gmail.com>
2026-06-28 08:02:02 +0300

    chore: update control panel config and clean support index

--- services/dsh/frontend/shared/support/index.ts
+++ services/dsh/frontend/shared/support/index.ts
@@ -3,6 +3,4 @@ export * from "./support.states";
 export { useSupportTicketController, useOperatorTicketController, useTicketDetailController, useSupportIncidentController } from "./use-support-controller";
 export * from "./support.flows";
 export * from "./support.captain-escalation";
-
-
-
+export * from "./support-registry";
