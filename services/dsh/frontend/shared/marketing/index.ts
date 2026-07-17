00379417fa65d2e80e8a69d86a1cd09b1b225baa
bthwani2-boop <242864241+bthwani2-boop@users.noreply.github.com>
2026-07-18 02:24:14 +0300

    fix(marketing): remove broken commercial program exports

--- services/dsh/frontend/shared/marketing/index.ts
+++ services/dsh/frontend/shared/marketing/index.ts
@@ -2,6 +2,4 @@ export * from "./marketing.types";
 export * from "./use-marketing-controller";
 export * from "./dsh-signal-layer.model";
 export * from "./marketing-registry";
-export * from "./loyalty-subscriptions.types";
-export * from "./use-commercial-programs-controller";
 export * from "./marketing.api";
