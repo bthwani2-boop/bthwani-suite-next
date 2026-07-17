5daf6beaded6ceea16e7b1604ea83a0a0111f81d
bthwani2-boop <242864241+bthwani2-boop@users.noreply.github.com>
2026-07-18 02:16:16 +0300

    feat(marketing): export live commercial program contracts

--- services/dsh/frontend/shared/marketing/index.ts
+++ services/dsh/frontend/shared/marketing/index.ts
@@ -2,4 +2,6 @@ export * from "./marketing.types";
 export * from "./use-marketing-controller";
 export * from "./dsh-signal-layer.model";
 export * from "./marketing-registry";
+export * from "./loyalty-subscriptions.types";
+export * from "./use-commercial-programs-controller";
 export * from "./marketing.api";
