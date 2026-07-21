8038292a388e62d50d468a9d50cd9eb7185a4d79
bthwani2-boop <242864241+bthwani2-boop@users.noreply.github.com>
2026-07-21 17:54:52 +0300

    feat(jrn-011/fs-11): export visible state policy

--- services/dsh/frontend/shared/order-truth/index.ts
+++ services/dsh/frontend/shared/order-truth/index.ts
@@ -2,5 +2,6 @@ export * from "./order-truth.types";
 export * from "./order-truth.api";
 export * from "./order-truth-create-attempt";
 export * from "./order-truth.view-model";
+export * from "./order-truth.visible-states";
 export * from "./use-order-truth-controller";
 export * from "./OrderTruthReadbackSummary";
