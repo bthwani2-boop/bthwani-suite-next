8aaea03cf975e9db8c108b4a0877fc53d6787ea2
bthwani2-boop <242864241+bthwani2-boop@users.noreply.github.com>
2026-07-21 21:38:37 +0300

    feat(jrn-003): export employee media binding

--- services/dsh/frontend/shared/workforce/index.ts
+++ services/dsh/frontend/shared/workforce/index.ts
@@ -2,6 +2,7 @@ export * from "./workforce.types";
 export * from "./workforce.api";
 export * from "./workforce-documents.api";
 export * from "./workforce-scopes.api";
+export * from "./workforce-employee-media.api";
 export * from "./use-workforce-controllers";
 export * from "./use-workforce-employee-controllers";
 export * from "./use-workforce-create-activation-controller";
