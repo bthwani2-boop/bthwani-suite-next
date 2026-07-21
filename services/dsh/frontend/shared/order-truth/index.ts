fb952c9fe02916365d17b88698d74349f4dcf70e
bthwani2-boop <242864241+bthwani2-boop@users.noreply.github.com>
2026-07-21 18:03:10 +0300

    feat(jrn-011/fs-14): export order truth experience policy

--- services/dsh/frontend/shared/order-truth/index.ts
+++ services/dsh/frontend/shared/order-truth/index.ts
@@ -3,5 +3,6 @@ export * from "./order-truth.api";
 export * from "./order-truth-create-attempt";
 export * from "./order-truth.view-model";
 export * from "./order-truth.visible-states";
+export * from "./order-truth.experience";
 export * from "./use-order-truth-controller";
 export * from "./OrderTruthReadbackSummary";
