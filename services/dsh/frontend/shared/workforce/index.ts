7c11d247a444b620338bc3c66f85e22ff83d2d1f
bthwani2-boop <242864241+bthwani2-boop@users.noreply.github.com>
2026-07-21 21:23:17 +0300

    feat(jrn-003): export employee workforce controllers

--- services/dsh/frontend/shared/workforce/index.ts
+++ services/dsh/frontend/shared/workforce/index.ts
@@ -1,6 +1,7 @@
 export * from "./workforce.types";
 export * from "./workforce.api";
 export * from "./use-workforce-controllers";
+export * from "./use-workforce-employee-controllers";
 export * from "./use-workforce-create-activation-controller";
 export * from "./use-workforce-profile";
 export * from "./WorkforceAccessGate";
