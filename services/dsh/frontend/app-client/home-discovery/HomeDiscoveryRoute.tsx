68b5be9bc9f917eb2e3633ad36b227b9045cd087
bthwani2-boop <242864241+bthwani2-boop@users.noreply.github.com>
2026-07-21 19:37:05 +0300

    feat(jrn-007): forward home marketing navigation

--- services/dsh/frontend/app-client/home-discovery/HomeDiscoveryRoute.tsx
+++ services/dsh/frontend/app-client/home-discovery/HomeDiscoveryRoute.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import { HomeDiscoveryScreen } from './HomeDiscoveryScreen';
+import React from "react";
+import { HomeDiscoveryScreen } from "./HomeDiscoveryScreen";
 
 type Props = {
   onStorePress?: ((storeId: string, slug: string) => void) | undefined;
   onSpecialCategoryPress?: ((nodeId: string) => void) | undefined;
+  onMarketingAction?: ((actionType: string, actionTarget: string) => void) | undefined;
 };
 
-export function HomeDiscoveryRoute({ onStorePress, onSpecialCategoryPress }: Props) {
-  return <HomeDiscoveryScreen onStorePress={onStorePress} onSpecialCategoryPress={onSpecialCategoryPress} />;
+export function HomeDiscoveryRoute({ onStorePress, onSpecialCategoryPress, onMarketingAction }: Props) {
+  return (
+    <HomeDiscoveryScreen
+      onStorePress={onStorePress}
+      onSpecialCategoryPress={onSpecialCategoryPress}
+      onMarketingAction={onMarketingAction}
+    />
+  );
 }
