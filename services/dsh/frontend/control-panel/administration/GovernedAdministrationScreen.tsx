762f6d3f1db1e89fb58414a1a71c37cd69757e81
bthwani2-boop <242864241+bthwani2-boop@users.noreply.github.com>
2026-07-21 19:42:27 +0300

    feat(jrn-031): mount governed role definition queue

--- services/dsh/frontend/control-panel/administration/GovernedAdministrationScreen.tsx
+++ services/dsh/frontend/control-panel/administration/GovernedAdministrationScreen.tsx
@@ -2,12 +2,14 @@
 
 import React from "react";
 import { AdministrationDashboardScreen } from "./AdministrationDashboardScreen";
+import { RoleDefinitionApprovalQueue } from "./RoleDefinitionApprovalQueue";
 import { RoleAssignmentApprovalQueue } from "./RoleAssignmentApprovalQueue";
 
 export function GovernedAdministrationScreen() {
   return (
     <>
       <AdministrationDashboardScreen />
+      <RoleDefinitionApprovalQueue />
       <RoleAssignmentApprovalQueue />
     </>
   );
