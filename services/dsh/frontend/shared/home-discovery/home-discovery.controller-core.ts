a96d2a791840422db59784a1cd7a4de5ec115c91
bthwani2-boop <2.42864241e+08+bthwani2-boop@users.noreply.github.com>
2026-08-27 21:35:19 +0000

    Remove orphaned home discovery controller helper

--- services/dsh/frontend/shared/home-discovery/home-discovery.controller-core.ts
+++ services/dsh/frontend/shared/home-discovery/home-discovery.controller-core.ts
@@ -1,17 +1,3 @@
-import { loadingState, type HomeDiscoveryState } from "./home-discovery.states";
 import type { DiscoveryFilterKind } from "./home-discovery.types";
 
-type HomeDiscoveryControllerModel = {
-  readonly state: HomeDiscoveryState;
-  readonly activeFilter: DiscoveryFilterKind;
-};
-
 export const HOME_DISCOVERY_INITIAL_FILTER: DiscoveryFilterKind = "all";
-
-export async function loadHomeDiscovery(
-  fetcher: () => Promise<HomeDiscoveryState>,
-  publish: (state: HomeDiscoveryState) => void,
-): Promise<void> {
-  publish(loadingState());
-  publish(await fetcher());
-}
